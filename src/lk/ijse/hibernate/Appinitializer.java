978c9f64805eaea1623c2a64f1dce9a42b79bab8
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2021-11-19 21:57:49 +0530

    Add to Details Many To Many

--- src/lk/ijse/hibernate/Appinitializer.java
+++ src/lk/ijse/hibernate/Appinitializer.java
@@ -1,7 +1,9 @@
 package lk.ijse.hibernate;
 
+import lk.ijse.hibernate.entity.Lecture;
 import lk.ijse.hibernate.entity.Owner;
 import lk.ijse.hibernate.entity.Pet;
+import lk.ijse.hibernate.entity.Subject;
 import lk.ijse.hibernate.utill.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -46,11 +48,32 @@ public class Appinitializer {
 //        transaction.commit();
 //        session.close();
 
+        //Many TO Many
+        Lecture lecturer = new Lecture();
+        lecturer.setlId("L001");
+        lecturer.setName("Saman");
+
+        Subject s1 = new Subject();
+        s1.setsId("S001");
+        s1.setsName("Java");
+
+        Subject s2 = new Subject();
+        s2.setsId("S002");
+        s2.setsName("Hibernate");
+
+        lecturer.getSubjectList().add(s1);
+        lecturer.getSubjectList().add(s2);
+
+        s1.getLectureList().add(lecturer);
+        s2.getLectureList().add(lecturer);
+
         Session session = FactoryConfiguration.getInstance().getSession();
 
         Transaction transaction = session.beginTransaction();
 
-
+        session.save(lecturer);
+        session.save(s1);
+        session.save(s2);
 
         transaction.commit();
         session.close();
