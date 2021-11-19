9d52d6db7e26990541f247275b3108a1cbf22923
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2021-11-19 20:20:38 +0530

    Add Data One to Many Relation

--- src/lk/ijse/hibernate/Appinitializer.java
+++ src/lk/ijse/hibernate/Appinitializer.java
@@ -1,5 +1,7 @@
 package lk.ijse.hibernate;
 
+import lk.ijse.hibernate.entity.Owner;
+import lk.ijse.hibernate.entity.Pet;
 import lk.ijse.hibernate.utill.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -8,9 +10,31 @@ public class Appinitializer {
 
     public static void main(String[] args) {
 
+        Owner owner = new Owner();
+        owner.setoId("0001");
+        owner.setoName("yasiru");
+
+        Pet pet = new Pet();
+        pet.setpId("P001");
+        pet.setpName("Dog");
+
+        Pet pet2 = new Pet();
+        pet2.setpId("P002");
+        pet2.setpName("Bird");
+
+        owner.getPetList().add(pet);
+        owner.getPetList().add(pet2);
+
+        pet.setOwner(owner);
+        pet2.setOwner(owner);
+
         Session session = FactoryConfiguration.getInstance().getSession();
 
         Transaction transaction = session.beginTransaction();
+
+        session.save(owner);
+        session.save(pet);
+        session.save(pet2);
 
 
         transaction.commit();
